e2913272efaa954987c8b60a5eb7eb8c1b0fd13f
YouQam <webteam@YouQam.local>
2020-07-06 15:55:36 +0200

    #28: add service to pass data

---
+++ src/app/services/game-service.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameServiceService {
+  coords: number[];
+
+  constructor() { }
+
+  // Pass string
+  private dataSource = new BehaviorSubject("Hello service!");
+  
+  serviceData = this.dataSource.asObservable();
+
+  changeData(data: any) {
+    this.dataSource.next(data);
+  }
+
+
+  // Pass coordinates
+  private coordsSelected = new BehaviorSubject<number[]>([123, 456]);
+
+  serviceCoords = this.coordsSelected.asObservable();
+
+  changeCoords(coords: number[]) {
+    this.coordsSelected.next(coords);
+  }
+
+
+    // Pass minor to update beacon loaction
+    private minorNoSource = new BehaviorSubject(undefined);
+  
+    serviceMinorNo = this.minorNoSource.asObservable();
+  
+    changeMinorNo(minorNo: number) {
+      this.minorNoSource.next(minorNo);
+    }
+}
